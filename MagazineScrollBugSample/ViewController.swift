f0e860771e2303d66d6d95e9ce988dace5e3826f
Ondřej Kycelt <ondrej@jacon.cz>
2020-02-12 16:07:10 +0800

    Initial Commit

---
+++ MagazineScrollBugSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagazineScrollBugSample
+//
+//  Created by Ondřej Kycelt on 12/02/2020.
+//  Copyright © 2020 Ondřej Kycelt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
